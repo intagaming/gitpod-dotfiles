4cbffc9f3fb0c6def8005d2df82476a15a8ca027
An Hoang <xuanan2001@pm.me>
2023-03-12 10:10:48 +0700

    init

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+/bin/sh .dotfiles/install.sh
+
+
+# Install neovim
+cd $(mktemp -d)
+
+URL="https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
+if test -n "$NEOVIM_VERSION"
+then
+    URL="https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim.appimage"
+fi
+
+curl -LO "$URL"
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract >/dev/null
+mkdir -p /home/gitpod/.local/bin
+ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
+
